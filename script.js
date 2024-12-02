ef115b98f37cb550cb525a928273f22d824c0a60
Jonatha Leite Santos <jonatha.eti@gmail.com>
2024-12-02 17:52:35 -0300

    Dia 02 - Grupo de console

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert('Olá mundo')
+// mostrar as opções de ações pode ser para console para desenvolvedor
+console.log('Olá mundo')
+
+//Esta parte do programa
+console.group('AppGrupo')
+console.log('Olá mundo')
+console.error('Erro 550')
+console.warn('Erro Usuário 300')
+console.groupEnd()
